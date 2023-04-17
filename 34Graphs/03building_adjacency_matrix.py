bc6ebc66adeb9bf8ccc22e4771fba94ed7b57cc4
matheus costa <matheuscosta.tech@gmail.com>
2023-04-17 10:27:28 -0300

    Building up graph system

--- 34Graphs/03building_adjacency_matrix.py
+++ 34Graphs/03building_adjacency_matrix.py
@@ -1,14 +1,28 @@
+def get_edges(f):
+    a = []
+    for line in f.readlines():
+        print(line)
+        line = line.replace("\n", "")
+        a.append(tuple(line.split(" ")))
+    return a
+
+
+def get_vertices_labels(a):
+    flattened = []
+    list(flattened.extend(item) for item in a)
+    flattened = set(flattened)
+    return sorted(flattened)
+
+
 def main():
     with open("34Graphs/in1.txt") as f:
         qv, qa = map(int, f.readline().split(" "))
-        a = []
-
-        for line in f.readlines():
-            print(line)
-            line = line.replace("\n", "")
-            a.append(tuple(line.split(" ")))
+        a = get_edges(f)
 
         print("a", a)
+        v = get_vertices_labels(a)
+
+        print("v", v)
 
 
 if __name__ == "__main__":
