767966743db998e04bb6317bd368e7e178d3690f
matheus costa <matheuscosta.tech@gmail.com>
2023-06-10 21:17:42 -0300

    Getting topological sorting

--- 34Graphs/06topological_sorting.py
+++ 34Graphs/06topological_sorting.py
@@ -1,15 +1,37 @@
 from graph import get_edges, get_adjacency_vector
+from queue import LifoQueue
+
+
+def topological_sorting(adj_vec):
+    order = []
+    de = [0] * len(adj_vec)
+
+    for e in adj_vec.values():
+        for w in e:
+            de[w - 1] += 1
+
+    degree_zero = LifoQueue()
+    for i, degree in enumerate(de):
+        if degree == 0:
+            degree_zero.put(i + 1)
+
+    while not degree_zero.empty():
+        v = degree_zero.get()
+        order.append(v)
+
+        for w in adj_vec[v]:
+            de[w - 1] -= 1
+
+            if de[w - 1] == 0:
+                degree_zero.put(w)
+    return order
+
 
 with open("34Graphs/in4.txt") as f:
     qv, qe = map(int, f.readline().strip().split(" "))
 
     edges = get_edges(f)
-
     adj_vec = get_adjacency_vector(edges, qv, is_directed=True)
-    print(adj_vec)
 
-    degree_zero = []
-    for e in adj_vec.values():
-        if len(e) == 0:
-            degree_zero += []
-    print(degree_zero)
+    order = topological_sorting(adj_vec)
+    print(order)
