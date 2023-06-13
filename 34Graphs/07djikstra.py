448daa64b9c3c896c8b787364e50e6769b284165
matheus costa <matheuscosta.tech@gmail.com>
2023-06-13 08:58:32 -0300

    Building adj list for weighted graph

---
+++ 34Graphs/07djikstra.py
@@ -0,0 +1,18 @@
+with open("34Graphs/in5.txt") as f:
+    first_line = f.readline()
+    n = int(first_line.strip().split()[0])
+    m = first_line.strip().split()[1]
+
+    adj_list = {}
+
+    for v in range(n):
+        adj_list[v] = []
+
+    for line in f:
+        u = int(line.strip().split()[0])
+        v = int(line.strip().split()[1])
+        p = int(line.strip().split()[2])
+
+        adj_list[u].append((v, p))
+
+    print(adj_list)
