253df7f8ff3f07520515fa111607298f408a46f1
matheus <matheuscosta.tech@gmail.com>
2024-04-20 15:00:20 -0300

    Calculating fitness

--- 37generic_algorithm/1optimizing_gp_function.py
+++ 37generic_algorithm/1optimizing_gp_function.py
@@ -8,14 +8,40 @@ def initialize_population(population_size, x_range, y_range):
         x = random.uniform(x_range[0], x_range[1])
         y = random.uniform(y_range[0], y_range[1])
         chromosome = (x, y)
-        population.append(chromosome)
+        individual = {"chromosome": chromosome}
+        population.append(individual)
+    return population
+
+
+def goldstein_price_function(x, y):
+    term1 = 1 + ((x + y + 1) ** 2) * (
+        19 - 14 * x + 3 * x**2 - 14 * y + 6 * x * y + 3 * y**2
+    )
+    term2 = 30 + ((2 * x - 3 * y) ** 2) * (
+        18 - 32 * x + 12 * x**2 + 48 * y - 36 * x * y + 27 * y**2
+    )
+    return term1 * term2
+
+
+def evaluate_fitness(population):
+    for individual in population:
+        x, y = individual["chromosome"]
+        fitness = goldstein_price_function(x, y)
+        individual["fitness"] = fitness
     return population
 
 
 # Exemplo de uso
 population_size = 50
-x_range = (-2, 2)  # Intervalo válido para x
-y_range = (-2, 2)  # Intervalo válido para y
+
+# intervalos comummente usados para x e y
+x_range = (-2, 2)
+y_range = (-2, 2)
 
 population = initialize_population(population_size, x_range, y_range)
+ic(population)
+
+print("-" * 50)
+
+population = evaluate_fitness(population)
 ic(population)
