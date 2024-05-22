6561d35b49340ab8fe39625263ec95fccef07ce4
matheus <matheuscosta.tech@gmail.com>
2024-05-21 21:30:37 -0300

    Built a perceptron

---
+++ 39perceptron/main.py
@@ -0,0 +1,79 @@
+"""
+Pela análise do processo de destilação fracionada do petróleo observou-se que determinado óleo
+poderia ser classificado em duas classes de pureza (P1 e P2)
+Utilizando o algoritmo supervisionado de Hebb para classificação de padrões, e assumindo-se a
+taxa de aprendizagem como 0,01, faça as seguintes atividades:
+ Execute 5 treinamentos para a rede Perceptron, iniciando-se os vetores de pesos (w)
+em cada treinamento com valores aleatórios entre zero e um.
+ Registre os resultados dos cinco treinamentos em uma tabela contendo:
+o Pesos iniciais
+o Pesos finais
+o Número de épocas
+"""
+
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+class Perceptron:
+    def __init__(self, input_size, learning_rate=0.01, max_epochs=500):
+        self.weights = np.linspace(0, 1, input_size)
+        self.bias = 0
+        self.learning_rate = learning_rate
+        self.epochs = 0
+        self.max_epochs = max_epochs
+
+    def activation_function(self, x):
+        return 1 if x >= 0 else 0
+
+    def predict(self, x):
+        z = np.dot(x, self.weights) + self.bias
+        # translate the activation function to the actual value
+        return np.where(self.activation_function(z) == 1, 1, -1)
+
+    def train(self, X, y):
+        for _ in range(self.max_epochs):
+            ready_to_stop = True
+            self.epochs += 1
+            for i in range(len(X)):
+                error = y[i] - self.predict(X[i])
+                self.weights += self.learning_rate * error * X[i]
+                self.bias += self.learning_rate * error
+                # print("-" * 50)
+                # print("error", error)
+                # print("self.bias", self.bias)
+                # print("self.weights", self.weights)
+                if error != 0:
+                    ready_to_stop = False
+            if ready_to_stop:
+                break
+        # print("self.epochs", self.epochs)
+
+
+def calculate_accuracy(model, X, y):
+    predictions = np.array([model.predict(x) for x in X])
+    accuracy = np.mean(predictions == y)
+    return accuracy
+
+
+df = pd.read_csv("39perceptron/training_data.txt")
+
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=1)
+
+p = Perceptron(input_size=3, learning_rate=0.01)
+
+X = train_df[["x1", "x2", "x3"]].values
+y = train_df["d"].values
+p.train(X, y)
+
+X = test_df[["x1", "x2", "x3"]].values
+y = test_df["d"].values
+accuracy = calculate_accuracy(p, X, y)
+print("Accuracy on test set:", accuracy)
+
+# print(">>>", p.predict([-0.3665, 0.0620, 5.9891]))
+# print(">>>", p.predict([-0.6508, 0.1097, 4.0009]))
+# print(">>>", p.predict([0.2626, 1.1476, 7.7985]))
